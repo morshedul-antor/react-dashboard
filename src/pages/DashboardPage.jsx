196f7e972dad41df07575ae8d446c1e6ac82f110
morshedul-antor <antor.morshedul@gmail.com>
2022-08-20 01:22:01 +0600

    feat: dashboard page add

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Dashboard, Footer, Navbar } from '../components'
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <Navbar />
+            <Dashboard />
+            <Footer />
+        </div>
+    )
+}
