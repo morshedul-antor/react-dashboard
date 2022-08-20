d7a023fc1fcd1bdad97a1957e5170769934f9499
morshedul-antor <antor.morshedul@gmail.com>
2022-08-20 18:07:36 +0600

    feat: root color add for toggle theme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
-import { faBars, faBell, faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faBell, faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 import cr7 from '../../assets/cr7.jpg'
 import logo from '../../assets/logo.png'
 import classes from './Navbar.module.css'
 
 export default function Navbar() {
+    const [select, setSelect] = useState(1)
+    const setTheme = (theme) => (document.documentElement.className = theme)
+
+    const toggleLight = () => {
+        setTheme('light')
+    }
+    const toggleDark = () => {
+        setTheme('dark')
+    }
+
     return (
         <div className={classes.navbar}>
             <img src={logo} alt="" />
@@ -16,7 +27,27 @@ export default function Navbar() {
             </div>
             <div>
                 <FontAwesomeIcon icon={faBell} />
-                <FontAwesomeIcon icon={faMoon} />
+                <div>
+                    <FontAwesomeIcon icon={select === 2 ? faSun : faMoon} />
+                    <div className={classes.themeToggle}>
+                        <p
+                            onClick={() => {
+                                toggleDark()
+                                setSelect(1)
+                            }}>
+                            <FontAwesomeIcon icon={faMoon} />
+                            Dark
+                        </p>
+                        <p
+                            onClick={() => {
+                                toggleLight()
+                                setSelect(2)
+                            }}>
+                            <FontAwesomeIcon icon={faSun} />
+                            Light
+                        </p>
+                    </div>
+                </div>
                 <span>Hi, Cristiano</span>
                 <img src={cr7} alt="" />
                 <FontAwesomeIcon icon={faBars} className={classes.menu} />
