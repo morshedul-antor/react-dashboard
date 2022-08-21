0d1d17009bfa13b7a03b97ce9e629026e7944302
morshedul-antor <antor.morshedul@gmail.com>
2022-08-22 02:25:53 +0600

    feat: private route add with auth provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useReducer } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { DashboardPage } from './pages'
+import { Auth, Theme } from './contexts/context'
+import { authState, authReducer } from './contexts/reducers/auth'
+import { themeState, themeReducer } from './contexts/reducers/theme'
+import { DashboardPage, HomePage } from './pages'
+import PrivateRoute from './routes/PrivateRoute'
 
 function App() {
+    const [stateAuth, dispatchAuth] = useReducer(authReducer, authState)
+    const [stateTheme, dispatchTheme] = useReducer(themeReducer, themeState)
+
     return (
-        <Routes>
-            <Route path="/" element={<DashboardPage />} />
-        </Routes>
+        <Auth.Provider value={{ stateAuth, dispatchAuth }}>
+            <Theme.Provider value={{ stateTheme, dispatchTheme }}>
+                <Routes>
+                    <Route path="/home" element={<HomePage />} />
+
+                    <Route path="/*" element={<PrivateRoute />}>
+                        <Route path="" element={<DashboardPage />} />
+                    </Route>
+                </Routes>
+            </Theme.Provider>
+        </Auth.Provider>
     )
 }
 
