f42656ee2a19b7d57be4111ce79f1065fd3bf53c
morshedul-antor <antor.morshedul@gmail.com>
2022-08-22 02:22:06 +0600

    feat: theme fetch from local storage

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,15 @@
 import { faBars, faBell, faChartSimple, faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState } from 'react'
+import { useContext } from 'react'
 import cr7 from '../../assets/cr7.jpg'
+import { Auth, Theme } from '../../contexts/context'
 import classes from './Navbar.module.css'
 
 export default function Navbar() {
-    const [select, setSelect] = useState(1)
-    const setTheme = (theme) => (document.documentElement.className = theme)
+    const { dispatchAuth } = useContext(Auth)
+    const { stateTheme, dispatchTheme } = useContext(Theme)
 
-    const toggleLight = () => {
-        setTheme('light')
-    }
-    const toggleDark = () => {
-        setTheme('dark')
-    }
+    document.documentElement.className = stateTheme?.mode
 
     return (
         <div className={classes.navbar}>
@@ -30,20 +26,18 @@ export default function Navbar() {
             <div>
                 <FontAwesomeIcon icon={faBell} />
                 <div>
-                    <FontAwesomeIcon icon={select === 2 ? faSun : faMoon} />
+                    <FontAwesomeIcon icon={stateTheme?.mode === 'light' ? faSun : faMoon} />
                     <div className={classes.themeToggle}>
                         <p
                             onClick={() => {
-                                toggleLight()
-                                setSelect(2)
+                                dispatchTheme({ type: 'LIGHT' })
                             }}>
                             <FontAwesomeIcon icon={faSun} />
                             Light
                         </p>
                         <p
                             onClick={() => {
-                                toggleDark()
-                                setSelect(1)
+                                dispatchTheme({ type: 'DARK' })
                             }}>
                             <FontAwesomeIcon icon={faMoon} />
                             Dark
@@ -51,7 +45,7 @@ export default function Navbar() {
                     </div>
                 </div>
                 <span>Hi, Cristiano</span>
-                <img src={cr7} alt="" />
+                <img src={cr7} alt="" title="Logout" onClick={() => dispatchAuth({ type: 'LOGOUT' })} />
                 <FontAwesomeIcon icon={faBars} className={classes.menu} />
             </div>
         </div>
